9b1263541ee455d7bd782b5ba0956877a39101fd
KiranTheRam <kramji12@gmail.com>
2021-12-01 15:37:25 -0500

    All search functions working

--- main.py
+++ main.py
@@ -2,44 +2,21 @@
 import json
 from urllib.request import urlopen, Request
 
-# These 2 were only ever pulling Asuna's info
-# def playerSearch(playerName):
-#     if [x for x in data['players'] if x.get('gameName') == playerName]:
-#         print("Found", playerName)
-#         print("-------\n", data['players']['gameName'], "\n-------")
-#         player = data['players']['gameName' == playerName]
-#         print("Ranking: ", player['leaderboardRank'])
-#         print("Rating: ", player['rankedRating'])
-#         print("Number of wins: ", player['numberOfWins'])
-#         print("Competitive Tier: ", player['competitiveTier'])
-#     else:
-#         print("Not Found")
-#
-# def rankSearch(rank):
-#     if [x for x in data['players'] if x.get('leaderboardRank') == rank]:
-#         print("Found")
-#         player = data['players']['leaderboardRank' == rank]
-#         print("Ranking: ", player['leaderboardRank'])
-#         print("Rating: ", player['rankedRating'])
-#         print("Number of wins: ", player['numberOfWins'])
-#         print("Competitive Tier: ", player['competitiveTier'])
-#     else:
-#         print("Not Found")
 
-def playerSearch(playerName):
+def playerSearch(passedPlayerName):
     for x in data['players']:
-        if x.get('gameName') == playerName:
+        if x.get('gameName') == passedPlayerName:
             print("Found", x.get('gameName'))
             print("Ranking: ", x.get('leaderboardRank'))
-            print("Type for rank object: ", type(x.get('leaderboardRank')))
             print("Rating: ", x.get('rankedRating'))
             print("Number of wins: ", x.get('numberOfWins'))
             print("Competitive Tier: ", x.get('competitiveTier'))
             break
 
-def rankSearch(rank):
+
+def rankSearch(passedRank):
     for x in data['players']:
-        if x.get('leaderboardRank') == rank:
+        if x.get('leaderboardRank') == passedRank:
             print("Found rank #", x.get('leaderboardRank'))
             print("Name: ", x.get('gameName'))
             print("Rating: ", x.get('rankedRating'))
@@ -48,11 +25,9 @@ def rankSearch(rank):
             break
 
 
-
-
 baseURL = "https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/[ACTID]?size=200&startIndex=0&api_key=[APIKEY]"
 currentActID = "a16955a5-4ad0-f761-5e9e-389df1c892fb"
-APIKey = "RGAPI-7d0c3d5f-452f-4f63-859d-b56e5bc7e927"
+APIKey = "Enter your own key scrub"
 URL = baseURL.replace("[ACTID]", currentActID)
 URL = URL.replace("[APIKEY]", APIKey)
 
@@ -70,17 +45,7 @@ try:
 except:
     print("Error encountered")
 
-# print(data)
-# print("\n\n", data['players'][0])
-
 # Based on command used in discord $p, $r, etc... a specific variable will be made to search against
-
-# TESTING - Creating a sub-dict for an individual player
-# player = data['players']['gameName' == "100T Asuna"]
-# print(player)
-# print(type(player))
-# print(player['leaderboardRank'])
-
 # WORKING
 while True:
     option = input("Do you want to search by 'player name' or 'rank', or 'exit: ")
@@ -88,15 +53,11 @@ while True:
 
     if option == "player name" or option == "playername":
         playerName = input("Enter a player name: ")
-        # playerSearch(playerName)
         playerSearch(playerName)
     elif option == "rank":
         rank = input("Enter a player rank: ")
-        rankSearch(rank)
+        rankSearch(int(rank))
     elif option == "exit":
         exit(1)
     else:
         print("Input did not match either option")
-
-# Not Used
-# print(any(sd['gameName']=='100T Asuna' for sd in data['players']))
